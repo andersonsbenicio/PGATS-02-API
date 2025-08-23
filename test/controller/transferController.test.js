997046c1cf9ca6813a186df1ef856326df379bd1
andersonsbenicio <andersonsbenicio@gmail.com>
2025-08-22 21:14:46 -0300

    Adicionando teste de API via HTTP na pipeline

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -16,12 +16,12 @@ describe("TransferController", () => {
       const resposta = await request(app).post("/transfers").send({
         from: "eustaquio",
         to: "crispiniana",
-        value: 100,
+        amount: 100,
       });
       expect(resposta.status).to.equal(400);
       expect(resposta.body).to.have.property(
         "error",
-        "Dados de transferência inválidos"
+        "Usuário remetente ou destinatário não encontrado"
       );
     });
 
@@ -33,7 +33,7 @@ describe("TransferController", () => {
       const resposta = await request(app).post("/transfers").send({
         from: "eustaquio",
         to: "crispiniana",
-        value: 100,
+        amount: 100,
       });
 
       expect(resposta.status).to.equal(400);
@@ -41,6 +41,31 @@ describe("TransferController", () => {
         "error",
         "Dados de transferência inválidos"
       );
+      //Reseto o Mock
+      sinon.restore();
+    });
+
+    it("Usando Mocks: Quando informo valores válidos eu tenho sucesso com 201 CREATED", async () => {
+      // Mocar apenas a função transfer do Service
+      const transferServiceMock = sinon.stub(transfService, "transfer");
+      transferServiceMock.returns({
+        from: "eustaquio",
+        to: "crispiniana",
+        amount: 100,
+        date: new Date().toDateString(),
+      });
+
+      const resposta = await request(app).post("/transfers").send({
+        from: "eustaquio",
+        to: "crispiniana",
+        amount: 100,
+      });
+
+      expect(resposta.status).to.equal(201);
+      expect(resposta.body).to.have.property("transfer");
+      expect(resposta.body.transfer).to.have.property("from", "eustaquio");
+      expect(resposta.body.transfer).to.have.property("to", "crispiniana");
+      expect(resposta.body.transfer).to.have.property("amount", 100);
 
       //Reseto o Mock
       sinon.restore();
